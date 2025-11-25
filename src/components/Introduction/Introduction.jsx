27afaaddef83b2586fb720e34e2ea733fa8179ae
eaves0118 <tranhieudz2024@gmail.com>
2025-11-25 16:18:24 +0700

    udpate main

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import styles from "./style.module.scss";
-const Introduction = () => {
-  return <div></div>;
+import classNames from "classnames";
+
+const Introduction = ({
+  title,
+  description,
+  img,
+  flexDirection = false,
+  subimg,
+}) => {
+  return (
+    <section
+      className={classNames({
+        [styles.introduction__reverse]: flexDirection,
+        [styles.introduction]: !flexDirection,
+      })}
+    >
+      <div className={styles.col__left}>
+        <img className={styles.intro__subimg} src={subimg} alt="" />
+        <h2 className={styles.intro__title}>{title}</h2>
+        <p className={styles.intro__des}>{description}</p>
+      </div>
+      <div className={styles.col__right}>
+        <img className={styles.intro__img} src={img} alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Introduction;
