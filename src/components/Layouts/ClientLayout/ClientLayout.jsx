ab2466a0dab5cccc067f121dd70941225c4f6a9e
eaves0118 <tranhieudz2024@gmail.com>
2025-12-08 20:21:23 +0700

    update

--- src/components/Layouts/ClientLayout/ClientLayout.jsx
+++ src/components/Layouts/ClientLayout/ClientLayout.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import ChatWidget from "./ChatWidget/ChatWidget";
+import DoctorChat from "./DoctorChat/DoctorChat";
+import PatientChat from "./PatientChat/PatientChat";
+
 import styles from "./style.module.scss";
 import { Outlet } from "react-router-dom";
 
 const ClientLayout = () => {
+  const userRole = "patient";
+
+  const [activeChat, setActiveChat] = useState(null);
+
+  const handleOpenDoctorChat = () => setActiveChat("doctor");
+  const handleOpenPatientChat = () => setActiveChat("patient");
+  const handleCloseChat = () => setActiveChat(null);
+
   return (
     <>
       <Header />
@@ -12,7 +26,31 @@ const ClientLayout = () => {
           <Outlet />
         </div>
       </div>
+
       <Footer />
+
+      <div className={styles.rightWidgetContainer}>
+        <ChatWidget
+          openDoctorChat={handleOpenDoctorChat}
+          openPatientChat={handleOpenPatientChat}
+        />
+
+        {/* DOCTOR CHAT */}
+        {userRole === "doctor" && (
+          <DoctorChat
+            isOpen={activeChat === "doctor"}
+            onClose={handleCloseChat}
+          />
+        )}
+
+        {/* PATIENT CHAT */}
+        {userRole === "patient" && (
+          <PatientChat
+            isOpen={activeChat === "patient"}
+            onClose={handleCloseChat}
+          />
+        )}
+      </div>
     </>
   );
 };
